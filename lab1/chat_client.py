d2579655de605454b0bf1244864e61cf8113e5c6
vjwua <62188903+vjwua@users.noreply.github.com>
2023-10-08 00:07:02 +0300

    Mini chatroom-2: Task 2

---
+++ lab1/chat_client.py
@@ -0,0 +1,34 @@
+import socket
+import threading
+
+nickname = input("Choose a nickname: ")
+
+HOST = "127.0.0.1"
+PORT = 25050
+
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client.connect((HOST, PORT))
+
+def receive():
+    while True:
+        try:
+            message = client.recv(1024).decode('ascii')
+            if message == 'NICK':
+                pass
+            else:
+                print(message)
+        except:
+            print("An error occurred!")
+            client.close()
+            break
+
+def write():
+    while True:
+        message = f'{nickname}: {input("")}'
+        client.send(message.encode('ascii'))
+
+receive_thread = threading.Thread(target=receive)
+receive_thread.start()
+
+write_thread = threading.Thread(target=write)
+write_thread.start()

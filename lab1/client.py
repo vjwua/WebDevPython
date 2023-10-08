e84a9beaac6a81c0d1d1d20676660df755c7c7d0
vjwua <62188903+vjwua@users.noreply.github.com>
2023-10-08 17:52:33 +0300

    Task 1 [3]

--- lab1/client.py
+++ lab1/client.py
@@ -6,9 +6,10 @@ PORT = 25050
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 sock.connect((HOST, PORT))
 
-data = input("Enter something to send: ")
+while input != "":
+    data = input("Enter something to send: ")
 
-sock.sendall(data.encode())
-data = sock.recv(1024)
+    sock.sendall(data.encode())
+    data = sock.recv(1024)
 
-print(f"Received {data!r}")
+    print(f"Received {data!r}")
--- lab1/server.py
+++ lab1/server.py
@@ -9,18 +9,20 @@ sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 sock.bind((HOST, PORT))
 sock.listen()
 conn, addr = sock.accept()
-print(f"Connected by {addr}")
+with conn:
+    print(f"Connected by {addr}")
+    while True:
+        data = conn.recv(1024)
+        if not data:
+            break
 
-while True:
-    data = conn.recv(1024)
-
-    print("Timeout is on...")
-    time.sleep(5)
-    print("Timeout is off...")
-    
-    conn.sendall(data)
-    print(f"\t[Message] - {data.decode()}")
-    print(f"\t[Time] - {datetime.now()}")
+        print("Timeout is on...")
+        time.sleep(5)
+        print("Timeout is off...")
+        
+        conn.sendall(data)
+        print(f"\t[Message] - {data.decode()}")
+        print(f"\t[Time] - {datetime.now()}")
 
 conn.close()
 sock.close()

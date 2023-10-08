5db9d45779570fda2603f0daf2bc65588c7ce33d
vjwua <62188903+vjwua@users.noreply.github.com>
2023-10-08 15:56:22 +0300

    Task 1 [2]

--- lab1/server.py
+++ lab1/server.py
@@ -1,5 +1,6 @@
 import socket
 from datetime import datetime
+import time
 
 HOST = "127.0.0.1"
 PORT = 25050
@@ -10,7 +11,16 @@ sock.listen()
 conn, addr = sock.accept()
 print(f"Connected by {addr}")
 
-data = conn.recv(1024)
-conn.sendall(data)
-print(f"\t[Message] - {data.decode()}")
-print(f"\t[Time] - {datetime.now()}")
+while True:
+    data = conn.recv(1024)
+
+    print("Timeout is on...")
+    time.sleep(5)
+    print("Timeout is off...")
+    
+    conn.sendall(data)
+    print(f"\t[Message] - {data.decode()}")
+    print(f"\t[Time] - {datetime.now()}")
+
+conn.close()
+sock.close()

72961f3844eff62bd958c53b55e4ff984e8cdbea
vjwua <62188903+vjwua@users.noreply.github.com>
2023-10-11 22:52:11 +0300

    Lab 2, Task 2

--- lab2/cgi-bin/form_handler.py
+++ lab2/cgi-bin/form_handler.py
@@ -1,7 +1,16 @@
 import cgi
 import html
+import http.cookies
+import os
 
 form = cgi.FieldStorage()
+
+cookie = http.cookies.SimpleCookie()
+
+if "HTTP_COOKIE" in os.environ:
+    cookie.load(os.environ["HTTP_COOKIE"])
+
+counter = int(cookie.get("counter", 0))
 
 try:
     username = form.getfirst("username", "admin")
@@ -14,14 +23,19 @@ try:
 
     if (region == "lv"):
         region_data = "Львівська"
+        counter += 1
     elif (region == "if"):
         region_data = "Івано-Франківська"
+        counter += 1
     elif (region == "te"):
         region_data = "Тернопільська"
+        counter += 1
     elif (region == "uzh"):
         region_data = "Закарпатська"
+        counter += 1
     elif (region == "cv"):
         region_data = "Чернівецька"
+        counter += 1
     else:
         region_data = "Регіон не обрано"
 
@@ -30,15 +44,30 @@ try:
     for group in groups:
         value_choice = form.getvalue(group, "off")
         groups_checkbox[group] = value_choice
+        if value_choice == "on":
+            counter += 1 
        
     if username == "admin" and password == "admin1234":
-        message = "вхід виконано успішно"
+        message = "Вхід виконано успішно"
     else:
-        message = "вхід не успішний, перевірте дані входу"
+        message = "Вхід не успішний, перевірте дані входу"
+
+    cookie["username"] = username
+    cookie["password"] = password
+    cookie["counter"] = str(counter)
+
+    print(f"Set-cookie: {cookie['username']};")
+    print(f"Set-cookie: {cookie['password']};")
+    print(f"Set-cookie: {cookie['counter']};")
+
+    if "delete_cookies" in form:
+        cookie["username"] = ""
+        cookie["password"] = ""
+        cookie["counter"] = ""
 
 except (NameError, KeyError) as e:
     message = "введіть дані для форми"
-    lang = None
+    region = None
     print(message)
 
 print("Content-type:text/html\r\n\r\n")
@@ -56,6 +85,11 @@ template_html = f"""
     <h2> {message} </h1>
     <h3> Регіон: {region_data} </h2>
     <h3> Група: {groups_checkbox=} </h2>
+    <h3> Кількість заповнених форм: {counter} </h3>
+    <h3> {os.environ["HTTP_COOKIE"]=} </h3>
+    <form method="POST">
+        <input type="submit" name="delete_cookies" value="Видалити cookies">
+    </form>
 </body>
 </html>
 """

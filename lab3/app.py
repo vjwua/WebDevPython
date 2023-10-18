83ac709cfc684388c2cf9963d0c5839078eb6f05
vjwua <62188903+vjwua@users.noreply.github.com>
2023-10-18 11:54:49 +0300

    Lab 3, Added user_data, done Homepage

--- lab3/app.py
+++ lab3/app.py
@@ -1,24 +1,44 @@
-from flask import Flask, render_template, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for
+import os
+from _datetime import datetime
+
 app = Flask(__name__)
-@app.route('/')
+
+def get_user_info():
+    user_os = os.name
+    user_agent = request.headers.get('User-Agent')
+    current_time = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+    return user_os, user_agent, current_time
+
+@app.route('/base')
 def index():
-    return render_template('base.html')
+    user_os, user_agent, current_time = get_user_info()
+    return render_template('base.html', user_os=user_os, user_agent=user_agent, current_time=current_time)
+
+@app.route('/home')
+@app.route('/')
+def home():
+    user_os, user_agent, current_time = get_user_info()
+    return render_template('home.html', user_os=user_os, user_agent=user_agent, current_time=current_time)
 
 @app.route('/cv')
 def cv():
-    return render_template('cv.html')
+    user_os, user_agent, current_time = get_user_info()
+    return render_template('cv.html', user_os=user_os, user_agent=user_agent, current_time=current_time)
 
 @app.route('/edu')
 def edu():
-    return render_template('edu.html')
+    user_os, user_agent, current_time = get_user_info()
+    return render_template('edu.html', user_os=user_os, user_agent=user_agent, current_time=current_time)
 
 @app.route('/hobbies')
 def hobbies():
-    return render_template('hobbies.html')
-
-if __name__ == '__main__':
-    app.run(debug=True)
+    user_os, user_agent, current_time = get_user_info()
+    return render_template('hobbies.html', user_os=user_os, user_agent=user_agent, current_time=current_time)
 
 @app.route("/main")
 def main():
     return redirect(url_for("base"))
+
+if __name__ == '__main__':
+    app.run(debug=True)

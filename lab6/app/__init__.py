3db163e002d6a529069ab52d2600e857ca99d0ed
vjwua <vjwivasyuk@gmail.com>
2023-11-02 10:26:57 +0200

    Lab 6, secret key changed

--- lab6/app/__init__.py
+++ lab6/app/__init__.py
@@ -1,7 +1,8 @@
 from flask import Flask
 
 app = Flask(__name__)
-app.secret_key = b"secret"
+app.secret_key = b"277764450344399279392461713642952840400"
+#using secrets.SystemRandom().getrandbits(128)
 
 from app import views
 from app import database

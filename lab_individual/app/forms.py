40814554514b9eef3aa7fbb922a4f61a2da7ab35
vjwua <vjwivasyuk@gmail.com>
2023-11-02 10:53:47 +0200

    Lab Ind, creating Feedback form

--- lab_individual/app/forms.py
+++ lab_individual/app/forms.py
@@ -1,5 +1,5 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, BooleanField, SubmitField
+from wtforms import StringField, PasswordField, BooleanField, SubmitField, SelectField
 from wtforms.validators import DataRequired, Length
 
 class LoginForm(FlaskForm):
@@ -26,3 +26,11 @@ class CreateTodoForm(FlaskForm):
     new_task = StringField("Задача", validators=[DataRequired("Це поле обовʼязкове"), Length(min=1, max=100)])
     description = StringField("Опис", validators=[DataRequired("Це поле обовʼязкове"), Length(min=1, max=200)])
     submit = SubmitField("Створити")
+
+class CreateFeedbackForm(FlaskForm):
+    title = StringField("Імʼя", validators=[DataRequired("Це поле обовʼязкове"), Length(min=1, max=100)])
+    description = StringField("Опишіть, що ви думаєте", validators=[DataRequired("Це поле обовʼязкове"), Length(min=1, max=200)])
+    rate = SelectField("Оцінка", choice=[
+        (1, 1), (2, 2), (3, 3), (4, 4), (5, 5)
+    ])
+    submit = SubmitField("Надіслати")

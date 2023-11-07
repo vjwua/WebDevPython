39546e893f2432be40ac59b2eee40776d37dee7f
vjwua <vjwivasyuk@gmail.com>
2023-11-07 18:08:38 +0200

    Lab 7, migration is done

--- lab7/app/database.py
+++ lab7/app/database.py
@@ -13,6 +13,16 @@ class Todo(db.Model):
     description = db.Column(db.String(200))
     complete = db.Column(db.Boolean)
 
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(20), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    image_file = db.Column(db.String(20), nullable=False, default='default.jpg')
+    password = db.Column(db.String(60), nullable=False)
+
+    def __repr__(self):
+        return f"User('{self.username}', '{self.email}', '{self.password}')"
+    
 with app.app_context():
     db.create_all()
 

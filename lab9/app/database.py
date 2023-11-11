ec6ee0ec4deb84cb9f219ab2295d749ec5b9bc14
vjwua <vjwivasyuk@gmail.com>
2023-11-11 09:35:33 +0200

    Lab 9, image_file fix

--- lab9/app/database.py
+++ lab9/app/database.py
@@ -22,14 +22,14 @@ class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(20), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
-    image_file = db.Column(db.String(20), nullable=False, default='default.jpg')
     password = db.Column(db.String(60), nullable=False)
+    image_file = db.Column(db.String(60), nullable=False, server_default='default.jpg')
 
-    def __init__(self, username, email, password, image_file='default.jpg'):
+    def __init__(self, username, email, password, image_file=None):
         self.username = username
         self.email = email
         self.password = bcrypt.generate_password_hash(password).decode('utf-8')
-        self.image_file = image_file
+        self.image_file = image_file or 'default.jpg'
 
     def validate_password(self, form_password):
         return bcrypt.check_password_hash(self.password, form_password)

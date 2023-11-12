8878ec80c4bc87dfa66a872e1552d1f65492d485
vjwua <vjwivasyuk@gmail.com>
2023-11-12 23:08:36 +0200

    Lab 9, little fixes 2

--- lab9/app/views.py
+++ lab9/app/views.py
@@ -246,7 +246,7 @@ def change_password():
 @login_required
 def todo():
     form = CreateTodoForm()
-    todo_list = db.session.query(Todo).all()
+    todo_list = Todo.query.all()
 
     return render_template('todo.html', form=form, todo_list=todo_list)
 

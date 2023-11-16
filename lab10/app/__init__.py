b33e1d80eadf925f449ba494a85e8b6c695eede8
vjwua <vjwivasyuk@gmail.com>
2023-11-16 20:08:43 +0200

    Lab 10, Fabric

--- lab10/app/__init__.py
+++ lab10/app/__init__.py
@@ -1,35 +1,32 @@
 from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
-from flask_migrate import Migrate
-from flask_bcrypt import Bcrypt
-from flask_login import LoginManager
-import os
+from .extensions import db, migrate, bcrypt, login_manager
+from config import config
 
 app = Flask(__name__)
-app.secret_key = b"277764450344399279392461713642952840400"
-#using secrets.SystemRandom().getrandbits(128)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv("DATABASE_URL", "sqlite:///flaskdb.db")
+def create_app(config_name = 'default'):
+    app = Flask(__name__)
+    app.config.from_object(config.get(config_name))
 
-db = SQLAlchemy(app)
-migrate = Migrate(app, db)
+    db.init_app(app)
+    migrate.init_app(app, db, render_as_batch=True)
+    login_manager.init_app(app)
+    bcrypt.init_app(app)
+    
+    with app.app_context():
+        from .home import home_blueprint
+        app.register_blueprint(home_blueprint, url_prefix='/')
 
-bcrypt = Bcrypt(app)
-login_manager = LoginManager(app)
-login_manager.login_view = 'login'
-login_manager.login_message_category = 'warning'
+        from .auth import auth_blueprint
+        app.register_blueprint(auth_blueprint, url_prefix='/auth')
 
-from .home import home_blueprint
-app.register_blueprint(home_blueprint, url_prefix='/')
+        from .account import account_blueprint
+        app.register_blueprint(account_blueprint, url_prefix='/account')
 
-from .auth import auth_blueprint
-app.register_blueprint(auth_blueprint, url_prefix='/auth')
+        from .todo import todo_blueprint
+        app.register_blueprint(todo_blueprint, url_prefix='/todo')
 
-from .account import account_blueprint
-app.register_blueprint(account_blueprint, url_prefix='/account')
+        from .cookies import cookies_blueprint
+        app.register_blueprint(cookies_blueprint, url_prefix='/cookies')
 
-from .todo import todo_blueprint
-app.register_blueprint(todo_blueprint, url_prefix='/todo')
-
-from .cookies import cookies_blueprint
-app.register_blueprint(cookies_blueprint, url_prefix='/cookies')
+        return app

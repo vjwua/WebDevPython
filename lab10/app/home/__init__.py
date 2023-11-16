90481bae0210d180d6fbf41c2708833c3da9269e
vjwua <vjwivasyuk@gmail.com>
2023-11-16 20:27:16 +0200

    Lab 10, static img for home

--- lab10/app/home/__init__.py
+++ lab10/app/home/__init__.py
@@ -1,5 +1,5 @@
 from flask import Blueprint
 
-home_blueprint = Blueprint('home_bp', __name__, template_folder="templates/home")
+home_blueprint = Blueprint('home_bp', __name__, template_folder="templates/home", static_folder="static/home")
 
 from . import views

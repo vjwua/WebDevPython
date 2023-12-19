5b9dca0bb5738ca64db6500512036f144bcfc8d0
vjwua <vjwivasyuk@gmail.com>
2023-12-19 10:21:41 +0200

    controlWork, post with list

--- controlWork/app/enterprises/views.py
+++ controlWork/app/enterprises/views.py
@@ -69,31 +69,40 @@ def get_all_enterprises():
 @enterprises_blueprint.route('/enterprises', methods=['POST'])
 @jwt_required()
 def post_enterprise():
-    new_data = request.get_json()
+    data_list = request.get_json()
 
-    if not new_data:
+    if not data_list:
         return jsonify({"message": "No input data provided"}), 400
     
-    if not new_data.get("title") or not new_data.get("description"):
-        return jsonify({"message": "There's no keys"}), 422 
+    enterprises = []
 
-    enterprise = Enterprise(
-        name=new_data.get('name'), 
-        activity_type=new_data.get('activity_type'),
-        number_of_workers=new_data.get('number_of_workers'),
-        revenue=new_data.get('revenue')
-    ),
-    
-    db.session.add(enterprise)
+    for new_data in data_list:
+        if not all(key in new_data for key in ["name", "activity_type", "number_of_workers", "revenue"]):
+            return jsonify({"message": "Missing keys in one or more entries. If data are correct, try to use square brackets"}), 422 
+
+        enterprise = Enterprise(
+            name=new_data['name'], 
+            activity_type=new_data['activity_type'],
+            number_of_workers=new_data['number_of_workers'],
+            revenue=new_data['revenue']
+        )
+
+        db.session.add(enterprise)
+        enterprises.append(enterprise)
+
     db.session.commit()
 
-    new_enterprise = Enterprise.query.filter_by(id=enterprise.id).first()
-    return jsonify(
-        {"id": new_enterprise.id, 
-         "name": new_enterprise.name, 
-         "activity_type": new_enterprise.activity_type,
-         "number_of_workers": new_enterprise.number_of_workers,
-         "revenue": new_enterprise.revenue}), 201
+    result = []
+    for new_enterprise in enterprises:
+        result.append({
+            "id": new_enterprise.id, 
+            "name": new_enterprise.name, 
+            "activity_type": new_enterprise.activity_type,
+            "number_of_workers": new_enterprise.number_of_workers,
+            "revenue": new_enterprise.revenue
+        })
+
+    return jsonify(result), 201
 
 @enterprises_blueprint.route('/enterprises/<int:id>', methods=['PUT'])
 @jwt_required()
